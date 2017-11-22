97429d44cf0675aa8121dca5750623c08c6a9995
armin.dizdar@gmail.com <jebenekipasvord1>
2017-11-22 08:18:06 +0100

    Added  TimePicker

--- app/src/main/java/com/dizdar/biggie/armin/tudu/TaskCollection.java
+++ app/src/main/java/com/dizdar/biggie/armin/tudu/TaskCollection.java
@@ -66,6 +66,56 @@ public class TaskCollection { //Beginning of TaskCollection body.
         return strDate;
     }
 
+   public String tasksInNextSixHours () {
+       String toReturn = null;
+       int tasksInSixHours = 0;
+       // Getting epoch time in milliseconds and converting to minutes.
+       long currentTimeInMinutes = (System.currentTimeMillis() / 1000) / 60;
+       //Adding 6 hours to current time value.
+       long timeInSixHours = currentTimeInMinutes + 300;
+
+
+        // Checking if the task date is today.
+       for (int i = 0; i < taskList.size(); i++) {
+           String toCompare = taskList.get(i).getDate();
+           if (today().equals(toCompare)) {
+
+        // If task date is today, then get hours and minutes from task.
+               int hour = taskList.get(i).getHour();
+               int minute = taskList.get(i).getMinute();
+               if (hour >12) {
+                   hour = hour - 12;
+               }
+        //Transforming hours to minutes,adding minutes to hour minutes and storing everything in long.
+               long minutesToCheck = Long.valueOf((hour * 60) + minute);
+
+
+        //Checking if value of current time and task time is less then value of 6 hours.
+               if ((currentTimeInMinutes + minutesToCheck) < timeInSixHours){
+
+        // Incrementing tasks in 6 hours counter.
+                   tasksInSixHours++;
+               } //Closing seconds if statement
+
+           } // Closing first if statement.
+
+       } //Closing for loop
+
+
+       // Defining which String should be returned depending on the value of tasksInSixHours
+       if (tasksInSixHours == 1) {
+           toReturn = "You have " + tasksInSixHours + " task in next 6 hours.";
+       }
+       else if (tasksInSixHours > 1) {
+           toReturn = "You have " + tasksInSixHours + " tasks in next 6 hours.";
+       }
+       else if (tasksInSixHours == 0) {
+           toReturn = "You don't have tasks scheduled in next 6 hours.";
+       }
+        return  toReturn;
+   } // Closing method body.
+
+
 } // End of TaskCollection body.
 
 
